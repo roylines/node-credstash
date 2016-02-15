cd28b6558064e2446d7d65d2d8e01d0410587cd1
roylines <roy@roylines.co.uk>
2016-02-15 20:26:41 +0000

    refactoring

--- index.js
+++ index.js
@@ -1,52 +1,19 @@
-const AWS = require('aws-sdk');
-const aesjs = require('aes-js');  
 const async = require('async');
-const crypto = require('crypto');
+const decrypter = require('./lib/decrypter');
 const encoder = require('./lib/encoder');
+const hmac = require('./lib/hmac');
+const keys = require('./lib/keys');
 const secrets = require('./lib/secrets');
 
 function Credstash(config) {
-  this.kms = new AWS.KMS();
-}
-
-function kmsDecrypt(kms, stash, done) {
-  var params = {
-    CiphertextBlob: encoder.decode(stash.key)
-  };
-
-  return kms.decrypt(params, (e, decrypted) => {
-    return done(e, stash, decrypted);
-  });
-}
-
-function splitKeys(stash, decrypted, done) {
-  stash.keyPlaintext = new Buffer(32);
-  stash.hmacPlaintext = new Buffer(32);
-  decrypted.Plaintext.copy(stash.keyPlaintext, 0, 0, 32);
-  decrypted.Plaintext.copy(stash.hmacPlaintext, 32);
-  return done(null, stash);
-}
-
-function checkHMAC(stash, done) {
-  // TODO - validate this
-  return done(null, stash);
-}
-
-function decrypt(stash, done) {
-  var key = stash.keyPlaintext;
-  var value = encoder.decode(stash.contents);
-  var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(1));
-  var decryptedBytes = aesCtr.decrypt(value);
-  return done(null, decryptedBytes.toString());
 }
 
 Credstash.prototype.get = function(name, done) {
   return async.waterfall([
     async.apply(secrets.get, name),
-    async.apply(kmsDecrypt, this.kms),
-    async.apply(splitKeys),
-    async.apply(checkHMAC),
-    async.apply(decrypt)
+    async.apply(keys.decrypt),
+    async.apply(hmac.check),
+    async.apply(decrypter.decrypt)
   ], done);
 };
 
