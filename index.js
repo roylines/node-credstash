1204041f46d4a22dc29fd56e5805a1af0aa5e5c3
roylines <roy@roylines.co.uk>
2016-02-17 14:36:17 +0000

    allow configuration of table name Closes #1

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const keys = require('./lib/keys');
 const secrets = require('./lib/secrets');
 
 function Credstash(config) {
+  this.table = config ? config.table : undefined;
 }
 
 Credstash.prototype.get = function(name, done) {
   return async.waterfall([
-    async.apply(secrets.get, name),
+    async.apply(secrets.get, this.table, name),
     async.apply(keys.decrypt),
     async.apply(hmac.check),
     async.apply(decrypter.decrypt)
--- test/index.js
+++ test/index.js
@@ -8,9 +8,22 @@ describe('credstash', () => {
   });
 
   it('can get secret', (done) => {
-    AWS.mock('DynamoDB', 'query', mockQuery); 
-    AWS.mock('KMS', 'decrypt', mockKMS); 
+    AWS.mock('DynamoDB', 'query', mockQuery('credential-store'));
+    AWS.mock('KMS', 'decrypt', mockKMS);
     var credstash = new Credstash();
+    return credstash.get('secret', (e, secret) => {
+      should.not.exist(e);
+      secret.should.equal('magic');
+      return done();
+    });
+  });
+
+  it('can get secret from alternative table', (done) => {
+    AWS.mock('DynamoDB', 'query', mockQuery('another-table'));
+    AWS.mock('KMS', 'decrypt', mockKMS);
+    var credstash = new Credstash({
+      table: 'another-table'
+    });
     return credstash.get('secret', (e, secret) => {
       should.not.exist(e);
       secret.should.equal('magic');
@@ -23,24 +36,27 @@ var mockKMS = (params, done) => {
   var ret = {
     Plaintext: new Buffer('KvQ7FPrc2uYXHjW8n+Y63HHCvyRjujeaIZepV/eUkfkz8ZbM9oymmzC69+XLTlbtvRV1MNmo3ngqE+7dJHoDMw==', 'base64')
   }
-  
+
   return done(null, ret);
 };
 
-var mockQuery = (params, done) => {
-  var ret = {
-    Items: [{
-      key: {
-        S: 'CiBzvX0zBm6hGu0EnbpRJ+eO+HfPOIsG5oq1UDiK+pi/vBLLAQEBAQB4c719MwZuoRrtBJ26USfnjvh3zziLBuaKtVA4ivqYv7wAAACiMIGfBgkqhkiG9w0BBwaggZEwgY4CAQAwgYgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMKNQYv5K9wPp+EvLQAgEQgFsITbvzf75MiY6aeIG2v/OzH2ThW5EJrfgNSekCGXONJSs3R8qkOlxFOfnoISvCXylMwBr+iAZFydgZiSyudPE+qocnYi++aVsv+iV9rR7o+FGQtSWKj2UH9PHm'
-      },
-      hmac: {
-        S: 'ada335c27410033b16887d083aba629c17ad8f88b7982f331e4f6f8df92c41a9'
-      },
-      contents: {
-        S: 'H2T+k+c='
-      }
-    }]
-  };
+var mockQuery = (expectedTable) => {
+  return (params, done) => {
+    params.TableName.should.equal(expectedTable);
+    var ret = {
+      Items: [{
+        key: {
+          S: 'CiBzvX0zBm6hGu0EnbpRJ+eO+HfPOIsG5oq1UDiK+pi/vBLLAQEBAQB4c719MwZuoRrtBJ26USfnjvh3zziLBuaKtVA4ivqYv7wAAACiMIGfBgkqhkiG9w0BBwaggZEwgY4CAQAwgYgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMKNQYv5K9wPp+EvLQAgEQgFsITbvzf75MiY6aeIG2v/OzH2ThW5EJrfgNSekCGXONJSs3R8qkOlxFOfnoISvCXylMwBr+iAZFydgZiSyudPE+qocnYi++aVsv+iV9rR7o+FGQtSWKj2UH9PHm'
+        },
+        hmac: {
+          S: 'ada335c27410033b16887d083aba629c17ad8f88b7982f331e4f6f8df92c41a9'
+        },
+        contents: {
+          S: 'H2T+k+c='
+        }
+      }]
+    };
 
-  return done(null, ret);
+    return done(null, ret);
+  };
 };
