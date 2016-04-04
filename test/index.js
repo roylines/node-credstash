bed6b1597be4c73e606417163219b2f6edd0d0db
José F. Romaniello <jfromaniello@gmail.com>
2016-04-04 10:01:36 -0300

    add support for retrieving the last N versions of a secret

--- test/index.js
+++ test/index.js
@@ -18,6 +18,19 @@ describe('credstash', () => {
     });
   });
 
+  it('can get N versions of a secret', (done) => {
+    AWS.mock('DynamoDB', 'query', mockQueryWithTake('credential-store'));
+    AWS.mock('KMS', 'decrypt', mockKMS);
+    var credstash = new Credstash();
+    return credstash.get('secret', { limit: 3 }, (e, secrets) => {
+      should.not.exist(e);
+      secrets[0].should.equal('magic');
+      secrets[1].should.equal('magic');
+      secrets[2].should.equal('magic');
+      return done();
+    });
+  });
+
   it('can get secret from alternative table', (done) => {
     AWS.mock('DynamoDB', 'query', mockQuery('another-table'));
     AWS.mock('KMS', 'decrypt', mockKMS);
@@ -45,6 +58,48 @@ var mockQuery = (expectedTable) => {
     params.TableName.should.equal(expectedTable);
     var ret = {
       Items: [{
+        key: {
+          S: 'CiBzvX0zBm6hGu0EnbpRJ+eO+HfPOIsG5oq1UDiK+pi/vBLLAQEBAQB4c719MwZuoRrtBJ26USfnjvh3zziLBuaKtVA4ivqYv7wAAACiMIGfBgkqhkiG9w0BBwaggZEwgY4CAQAwgYgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMKNQYv5K9wPp+EvLQAgEQgFsITbvzf75MiY6aeIG2v/OzH2ThW5EJrfgNSekCGXONJSs3R8qkOlxFOfnoISvCXylMwBr+iAZFydgZiSyudPE+qocnYi++aVsv+iV9rR7o+FGQtSWKj2UH9PHm'
+        },
+        hmac: {
+          S: 'ada335c27410033b16887d083aba629c17ad8f88b7982f331e4f6f8df92c41a9'
+        },
+        contents: {
+          S: 'H2T+k+c='
+        }
+      }]
+    };
+
+    return done(null, ret);
+  };
+};
+
+var mockQueryWithTake = (expectedTable) => {
+  return (params, done) => {
+    params.Limit.should.equal(3);
+    params.TableName.should.equal(expectedTable);
+    var ret = {
+      Items: [{
+        key: {
+          S: 'CiBzvX0zBm6hGu0EnbpRJ+eO+HfPOIsG5oq1UDiK+pi/vBLLAQEBAQB4c719MwZuoRrtBJ26USfnjvh3zziLBuaKtVA4ivqYv7wAAACiMIGfBgkqhkiG9w0BBwaggZEwgY4CAQAwgYgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMKNQYv5K9wPp+EvLQAgEQgFsITbvzf75MiY6aeIG2v/OzH2ThW5EJrfgNSekCGXONJSs3R8qkOlxFOfnoISvCXylMwBr+iAZFydgZiSyudPE+qocnYi++aVsv+iV9rR7o+FGQtSWKj2UH9PHm'
+        },
+        hmac: {
+          S: 'ada335c27410033b16887d083aba629c17ad8f88b7982f331e4f6f8df92c41a9'
+        },
+        contents: {
+          S: 'H2T+k+c='
+        }
+      },{
+        key: {
+          S: 'CiBzvX0zBm6hGu0EnbpRJ+eO+HfPOIsG5oq1UDiK+pi/vBLLAQEBAQB4c719MwZuoRrtBJ26USfnjvh3zziLBuaKtVA4ivqYv7wAAACiMIGfBgkqhkiG9w0BBwaggZEwgY4CAQAwgYgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMKNQYv5K9wPp+EvLQAgEQgFsITbvzf75MiY6aeIG2v/OzH2ThW5EJrfgNSekCGXONJSs3R8qkOlxFOfnoISvCXylMwBr+iAZFydgZiSyudPE+qocnYi++aVsv+iV9rR7o+FGQtSWKj2UH9PHm'
+        },
+        hmac: {
+          S: 'ada335c27410033b16887d083aba629c17ad8f88b7982f331e4f6f8df92c41a9'
+        },
+        contents: {
+          S: 'H2T+k+c='
+        }
+      },{
         key: {
           S: 'CiBzvX0zBm6hGu0EnbpRJ+eO+HfPOIsG5oq1UDiK+pi/vBLLAQEBAQB4c719MwZuoRrtBJ26USfnjvh3zziLBuaKtVA4ivqYv7wAAACiMIGfBgkqhkiG9w0BBwaggZEwgY4CAQAwgYgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMKNQYv5K9wPp+EvLQAgEQgFsITbvzf75MiY6aeIG2v/OzH2ThW5EJrfgNSekCGXONJSs3R8qkOlxFOfnoISvCXylMwBr+iAZFydgZiSyudPE+qocnYi++aVsv+iV9rR7o+FGQtSWKj2UH9PHm'
         },
