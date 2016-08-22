4e6629e353c71160d3444fdbf13b720f9e329745
David Tanner <david.joel.tanner@gmail.com>
2016-08-22 13:25:51 -0600

    Add function to get a secret by version and context If a context is used to encrypt the key in KMS, now it can be decrypted properly. Specific versions of a secret can be specified

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+'use strict';
+
 const async = require('async');
 const decrypter = require('./lib/decrypter');
 const encoder = require('./lib/encoder');
 const hmac = require('./lib/hmac');
-const keys = require('./lib/keys');
+const Keys = require('./lib/keys');
 const Secrets = require('./lib/secrets');
 const xtend = require('xtend');
+const utils = require('./lib/utils');
 
 const defaults = {
   limit: 1
@@ -15,8 +18,29 @@ function Credstash(config) {
   var awsConfig = config.awsConfig || {};
 
   this.secrets = new Secrets(config.table, awsConfig);
+  this.keys = new Keys(awsConfig);
 
 }
+
+Credstash.prototype.getSecret = function(name, version, context, done) {
+  let args = utils.optArgs({version:version, context: context, done: done});
+  version = args.version;
+  context = args.context;
+  done = args.done;
+
+  let fn = this.secrets.getLatestVersion.bind(this.secrets, name);
+  if (version) {
+    fn = this.secrets.getByVersion.bind(this.secrets, name, version);
+  }
+
+  return this.decryptSecrets(fn, context, function(err, secret) {
+    if (err) {
+      return done(err);
+    }
+
+    return done(null, secret[0] || secret)
+  });
+};
 
 Credstash.prototype.get = function(name, options, done) {
   if (typeof options === 'function') {
@@ -26,12 +50,8 @@ Credstash.prototype.get = function(name, options, done) {
     options = xtend(defaults, options);
   }
 
-  return async.waterfall([
-    async.apply(this.secrets.getVersions.bind(this.secrets), name, options),
-    async.apply(keys.decrypt),
-    async.apply(hmac.check),
-    async.apply(decrypter.decrypt)
-  ], function (err, secrets) {
+  return this.decryptSecrets(this.secrets.getVersions.bind(this.secrets, name, options), null,
+    function (err, secrets) {
     if (err) {
       return done(err);
     }
@@ -42,6 +62,15 @@ Credstash.prototype.get = function(name, options, done) {
 
     done(null, secrets);
   });
+};
+
+Credstash.prototype.decryptSecrets = function(getSecrets, context, done) {
+  return async.waterfall([
+    async.apply(getSecrets),
+    async.apply(this.keys.decryptAll.bind(this.keys, context)),
+    async.apply(hmac.check),
+    async.apply(decrypter.decrypt)
+  ], done);
 };
 
 module.exports = Credstash;
--- test/index.js
+++ test/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const Credstash = require('../index.js');
 const should = require('chai').should();
 const AWS = require('aws-sdk-mock');
@@ -43,14 +45,76 @@ describe('credstash', () => {
       return done();
     });
   });
+  
+  it('can handle args out of order', (done) => {
+    var context = {key: 'value'};
+    AWS.mock('DynamoDB.DocumentClient', 'get', mockGet('credential-store', 'version'));
+    AWS.mock('KMS', 'decrypt', mockKMSContext(context));
+    var credstash = new Credstash();
+    return credstash.getSecret('secret', context, 'version', (e, secret) => {
+      should.not.exist(e);
+      secret.should.equal('magic');
+      return done();
+    });
+  });
+
+  it('can get version X of a secret', (done) => {
+    AWS.mock('DynamoDB.DocumentClient', 'get', mockGet('credential-store', 'version'));
+    AWS.mock('KMS', 'decrypt', mockKMS);
+    var credstash = new Credstash();
+    return credstash.getSecret('secret', 'version', (e, secret) => {
+      should.not.exist(e);
+      secret.should.equal('magic');
+      return done();
+    });
+  });
+  
+  it('can uses context for a secret key', (done) => {
+    var context = {key: 'value'};
+    AWS.mock('DynamoDB.DocumentClient', 'get', mockGet('credential-store', 'version'));
+    AWS.mock('KMS', 'decrypt', mockKMSContext(context));
+    var credstash = new Credstash();
+    return credstash.getSecret('secret', 'version', context, (e, secret) => {
+      should.not.exist(e);
+      secret.should.equal('magic');
+      return done();
+    });
+  });
 });
 
-var mockKMS = (params, done) => {
-  var ret = {
-    Plaintext: new Buffer('KvQ7FPrc2uYXHjW8n+Y63HHCvyRjujeaIZepV/eUkfkz8ZbM9oymmzC69+XLTlbtvRV1MNmo3ngqE+7dJHoDMw==', 'base64')
-  };
+var kmsDecrypted = {
+  Plaintext: new Buffer('KvQ7FPrc2uYXHjW8n+Y63HHCvyRjujeaIZepV/eUkfkz8ZbM9oymmzC69+XLTlbtvRV1MNmo3ngqE+7dJHoDMw==', 'base64')
+};
 
-  return done(null, ret);
+var mockKMS = (params, done) => {
+  return done(null, kmsDecrypted);
+};
+
+var mockKMSContext = (expectedContext) => {
+  return (params, done) => {
+    if (expectedContext) {
+      params.EncryptionContext.should.eql(expectedContext);
+    }
+    return done(null, kmsDecrypted);
+  };
+};
+
+var mockGet = (expectedTable, expectedVersion) => {
+  return (params, done) => {
+    params.TableName.should.equal(expectedTable);
+    if (expectedVersion) {
+      params.Key.version.should.equal(expectedVersion);
+    }
+    var ret = {
+      Item: {
+        key: 'CiBzvX0zBm6hGu0EnbpRJ+eO+HfPOIsG5oq1UDiK+pi/vBLLAQEBAQB4c719MwZuoRrtBJ26USfnjvh3zziLBuaKtVA4ivqYv7wAAACiMIGfBgkqhkiG9w0BBwaggZEwgY4CAQAwgYgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMKNQYv5K9wPp+EvLQAgEQgFsITbvzf75MiY6aeIG2v/OzH2ThW5EJrfgNSekCGXONJSs3R8qkOlxFOfnoISvCXylMwBr+iAZFydgZiSyudPE+qocnYi++aVsv+iV9rR7o+FGQtSWKj2UH9PHm',
+        hmac: 'ada335c27410033b16887d083aba629c17ad8f88b7982f331e4f6f8df92c41a9',
+        contents:'H2T+k+c='
+      }
+    };
+
+    return done(null, ret);
+  };
 };
 
 var mockQuery = (expectedTable) => {
