923122f7fa75b054aed705eca54d39e0f8fb3159
Brendan Hurley <brendan@creativebench.co.uk>
2019-06-26 13:42:26 +0100

    overloading for optionals

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,19 @@
 export = Credstash;
 
 declare class Credstash {
-  public list(options: Credstash.IConfig, callback: Credstash.Callback<string[]>): void;
-  public get(value: string, options: Credstash.IConfig, callback: Credstash.Callback<string>): void;
+  list(options: Credstash.IOptions, callback: Credstash.Callback<string[]>): void;
+  list(callback: Credstash.Callback<string[]>): void;
+
+  get(value: string, options: Credstash.IOptions, callback: Credstash.Callback<string>): void;
+  get(value: string, callback: Credstash.Callback<string>): void;
 }
 
 declare namespace Credstash {
   type Callback<T> = (err: Error | null, response: T) => void;
 
-  interface IConfig {
+  interface IOptions {
     limit?: number,
     region?: string,
     table?: string
-  };
+  }
 }
